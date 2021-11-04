b9fbe01bcb595b097c10bc2db99363d86c135408
Mark Nadig <MarkNadig@Deltek.com>
2021-11-04 10:24:59 -0600

    First commit

---
+++ AuthenticationService/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthenticationService
+{
+    public class LoginModel
+    {
+        public string Identity { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MyService/Authenticator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Flurl;
+using AuthenticationService;
+
+namespace MyService
+{
+    public class Authenticator
+    {
+        ILogger _logger;
+
+        public Authenticator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<LoginResponse> GetClientLoginToken(string clientToken, string clientSecret)
+        {
+            var model = new LoginModel() { Identity = clientToken, Password = clientSecret};
+
+            using (var client = new HttpClient())
+            {
+                var uri = "localhost".AppendPathSegment("api/ClientAuthenticate/login");
+                var response = await client.PostAsJsonAsync(uri, model);
+                if (!response.IsSuccessStatusCode) throw new ApplicationException("Unable to retrieve access token");
+                return await response.Content.ReadFromJsonAsync<LoginResponse>();
+            }
+        }
+
+    }
+}
